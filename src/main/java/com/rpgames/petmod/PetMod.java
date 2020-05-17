796f1e73ba9307fb50dcf3c5f887e6ea4b2a8e10
vblimov <vblimov@gmail.com>
2020-05-17 14:32:07 +1000

    recommit

--- src/main/java/com/rpgames/petmod/PetMod.java
+++ src/main/java/com/rpgames/petmod/PetMod.java
@@ -1,11 +1,13 @@
 package com.rpgames.petmod;
 
+import com.rpgames.petmod.client.renders.RaccoonEntityRender;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -51,6 +53,7 @@ public class PetMod
     }
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
+        RenderingRegistry.registerEntityRenderingHandler(RegistryHandler.RACCOON_ENTITY.get(), RaccoonEntityRender::new);
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
     // Using for other mods compability
