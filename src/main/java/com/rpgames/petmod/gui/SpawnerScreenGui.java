9bcdf4a12c5063d693925fae644d58a4456e607c
Runo <31756050+RunoLight@users.noreply.github.com>
2020-05-22 16:13:58 +1000

    reworked gui spawner

--- src/main/java/com/rpgames/petmod/gui/SpawnerScreenGui.java
+++ src/main/java/com/rpgames/petmod/gui/SpawnerScreenGui.java
@@ -1,24 +1,27 @@
 package com.rpgames.petmod.gui;
 
-import com.mojang.blaze3d.platform.GlStateManager;
 import com.rpgames.petmod.PetMod;
 import com.rpgames.petmod.network.Networking;
 import com.rpgames.petmod.network.PacketSpawn;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 
 public class SpawnerScreenGui extends Screen {
 
-    // size of spawner_gui.png
-    private static final int WIDTH = 179;
-    private static final int HEIGHT = 151;
-
+    // Background texture
     private final ResourceLocation GUI = new ResourceLocation(PetMod.MOD_ID, "textures/gui/spawner_gui.png");
+    //private static final int WIDTH = 179;
+    //private static final int HEIGHT = 151;
+
+    private static final int WIDTH = 180;
+    private static final int HEIGHT = 148;
+
+    private final String summonVillager = "/summon villager ~ ~1 ~ {VillagerData:{profession:\"rpgpetmod:pet_villager\",level:2,type:plains}}";
+    private final String cleanEntities = "/kill @e[distance=..20, type=!minecraft:player]";
+    private final String herobrinePrank = "/tellraw @a {\"text\":\"Herobrine joined the game\",\"color\":\"yellow\"}";
 
     public SpawnerScreenGui() {
         super(new StringTextComponent("SpawnSomething"));
@@ -30,44 +33,73 @@ public class SpawnerScreenGui extends Screen {
         int relX = (this.width - WIDTH) / 2;
         int relY = (this.height - HEIGHT) / 2;
 
-        addButton(new Button(relX + 10, relY + 10, 160, 20, "Raccoon", button -> spawn(PetMod.MOD_ID + ":" + "raccoon_entity", 100)));
-        addButton(new Button(relX + 10, relY + 37, 160, 20, "Zombie", button -> spawn("minecraft:zombie")));
+        addButton(new Button(relX + 130  + 20, relY + 10, 20, 20, "X", button -> hideGui()));
+        addButton(new Button(relX + 10, relY + 10, 135, 20, "Clean", button -> executeCommand(cleanEntities)));
+
+        addButton(new Button(relX + 10,  relY + 118, 160,20, "Raccoon", button -> spawn(PetMod.MOD_ID, "raccoon_entity")));
+        addButton(new Button(relX + 10, relY + 37, 160, 20, "Pet keeper", button -> executeCommand(summonVillager)));
         addButton(new Button(relX + 10, relY + 64, 160, 20, "Cow", button -> spawn("minecraft:cow")));
         addButton(new Button(relX + 10, relY + 91, 160, 20, "Sheep", button -> spawn("minecraft:sheep")));
-        addButton(new Button(relX + 10, relY + 118, 160, 20, "Chicken", button -> spawn("minecraft:chicken")));
+
     }
 
-    public boolean isPauseScreen() {
-        return false;
-    }
 
+
+    /*
+     Commands
+     */
+
+    // Example arguments: ("minecraft:zombie")
     private void spawn(String id) {
         if (minecraft != null) {
+            assert minecraft.player != null;
             Networking.INSTANCE.sendToServer(new PacketSpawn(id, minecraft.player.dimension, minecraft.player.getPosition()));
-            minecraft.displayGuiScreen(null);
-        }
-    }
-    private void spawn(String id, int value) {
-        if (minecraft != null) {
-            for(int i = 0; i<value; i++) {
-                Networking.INSTANCE.sendToServer(new PacketSpawn(id, minecraft.player.dimension, minecraft.player.getPosition()));
-                minecraft.displayGuiScreen(null);
-            }
         }
     }
 
+    // Example arguments: ("minecraft", "zombie")
+    private void spawn(String modId, String id) {
+        if (minecraft != null) {
+            assert minecraft.player != null;
+            StringBuilder sb = new StringBuilder();
+            String mobId = sb.append(modId).append(':').append(id).toString();
+            Networking.INSTANCE.sendToServer(new PacketSpawn(mobId, minecraft.player.dimension, minecraft.player.getPosition()));
+        }
+    }
+
+    private void executeCommand(String command) {
+        if (minecraft != null) {
+            assert minecraft.player != null;
+            minecraft.player.sendChatMessage(command);
+        }
+    }
+
+    private void hideGui() {
+        assert minecraft != null;
+        minecraft.displayGuiScreen(null);
+    }
+     /*
+      --Commands
+      */
+
+    // Bind background picture
     @Override
     public void render(int mouseX, int mouseY, float partialTicks) {
         if (this.minecraft != null) {
             this.minecraft.getTextureManager().bindTexture(GUI);
             int relX = (this.width + WIDTH) / 2;
             int relY = (this.height - HEIGHT) / 2;
-            this.blit(relX - WIDTH, relY, 0,0,WIDTH, HEIGHT);
+            this.blit(relX - WIDTH, relY, 0,0, WIDTH, HEIGHT);
             super.render(mouseX, mouseY, partialTicks);
         }
     }
 
+    // Opens gui
     public static void open() {
         Minecraft.getInstance().displayGuiScreen(new SpawnerScreenGui());
+    }
+
+    public boolean isPauseScreen() {
+        return false;
     }
 }
