e492c7c18137a68c4240ccbcc37122d52a9592d3
Runo <31756050+RunoLight@users.noreply.github.com>
2020-05-23 14:22:39 +1000

    Simple item use example with sound effect

--- src/main/java/com/rpgames/petmod/item/SimpleItem.java
+++ src/main/java/com/rpgames/petmod/item/SimpleItem.java
@@ -2,11 +2,20 @@ package com.rpgames.petmod.item;
 
 import com.rpgames.petmod.PetMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundEvents;
 
 public class SimpleItem extends Item {
 
     public SimpleItem() {
         super(new Item.Properties()
                 .group(PetMod.petModItemGroup));
+    }
+
+    @Override
+    public ActionResultType onItemUse(ItemUseContext context) {
+        context.getPlayer().playSound(SoundEvents.ENTITY_WANDERING_TRADER_YES, 1.0F, 1.0F);
+        return super.onItemUse(context);
     }
 }
